3f2c67f3c4709f9bc41cfdcf7562576f57033a5d
kiriha1203 <mizumoto.takefumi@gmail.com>
2021-06-14 00:00:18 +0900

    Add 状態の検索機能の追加

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@
       <button class="button button--green" @click="insert">追加</button>
     </div>
     <div class="Filter">
-      <button class="button button--gray is-active">全て</button>
-      <button class="button button--gray">作業前</button>
-      <button class="button button--gray">作業中</button>
-      <button class="button button--gray">完了</button>
+      <button class="button button--gray" v-bind:class="{'is-active':(!find_flg)}" @click="flag_reset">全て</button>
+      <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '作業前')}" @click="find('作業前')">作業前</button>
+      <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '作業中')}" @click="find('作業中')">作業中</button>
+      <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '完了')}" @click="find('完了')">完了</button>
     </div>
     <table class="Lists">
       <thead>
@@ -21,7 +21,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(todo, index) in todos" :key="index">
+        <tr v-for="(todo, index) in display_todos" :key="index">
           <td>{{ todo.content }}</td>
           <td>{{ todo.created }}</td>
           <td>
@@ -47,11 +47,27 @@ import {mapState} from 'vuex';
 export default {
   data: function() {
     return {
-      content: ''
+      content: '',
+      find_state: '',
+      find_flg: false
     }
   },
   computed: {
-    ...mapState(['todos'])
+    ...mapState(['todos']),
+    display_todos:function() {
+      if(this.find_flg) {
+        let arr = [];
+        let data = this.todos;
+        data.forEach(element =>{
+          if(element.state == this.find_state) {
+            arr.push(element);
+          }
+        });
+        return arr;
+      } else {
+        return this.todos;
+      }
+    }
   },
   methods: {
     insert: function() {
@@ -63,8 +79,15 @@ export default {
     remove: function(todo) {
       this.$store.commit('remove', todo)
     },
-    changeState: function(todo){
+    changeState: function(todo) {
       this.$store.commit('changeState', todo)
+    },
+    find: function(findState) {
+      this.find_state = findState;
+      this.find_flg = true;
+    },
+    flag_reset: function() {
+      this.find_flg = false;
     }
   }
 }
